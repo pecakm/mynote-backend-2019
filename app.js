0e9f1658ed8c80e7f6845132362e3fdb32ac4e75
Mikolaj Pecak <mikolajpecak@gmail.com>
2019-02-12 21:45:48 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const CONSTANTS = require('./api/helpers/constants');
+const reportRoutes = require('./api/routes/reports');
+const userRoutes = require('./api/routes/users');
+const poiRoutes = require('./api/routes/pois');
+const authRoutes = require('./api/routes/auth');
+
+mongoose.connect(CONSTANTS.DATABASE_URL, {
+    useNewUrlParser: true
+});
+
+const app = express();
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
+
+app.use('/reports', reportRoutes);
+app.use('/users', userRoutes);
+app.use('/pois', poiRoutes);
+app.use('/auth', authRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
+
+module.exports = app;
